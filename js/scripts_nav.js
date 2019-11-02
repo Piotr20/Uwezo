5f7d2b663b09fe58e9170e48123486642494bf69
Piotr20 <piotrpospiech00@gmail.com>
2019-11-03 00:04:21 +0100

    section and footer

--- js/scripts_nav.js
+++ js/scripts_nav.js
@@ -12,3 +12,26 @@ X.addEventListener('click', function () {
      curtain.classList.add('out');
      curtain.classList.add('dis');
 });
+
+let b = 0;
+
+function colorChange() {
+
+     b = window.scrollY || window.pageYOffset;
+     let nav1 = document.querySelector('nav a:nth-of-type(1)');
+     let nav2 = document.querySelector('nav a:nth-of-type(2)');
+     let nav3 = document.querySelector('nav a:nth-of-type(3)');
+
+
+     if (b > 999 && b < 1900) {
+          nav1.style.color = "white";
+          nav2.style.color = "white";
+          nav3.style.color = "white";
+     } else {
+          nav1.style.color = "#3e3e3e";
+          nav2.style.color = "#3e3e3e";
+          nav3.style.color = "#3e3e3e";
+
+     }
+}
+window.addEventListener('scroll', colorChange);
